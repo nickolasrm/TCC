5b03bd6816bef4dfabaa598c941cee4b168fe4b7
nickolasrm <nickolasrochamachado@gmail.com>
2023-02-05 00:55:51 +0000

    fix: :bug: fixed postInstallScript not installing package

--- .devcontainer/postInstallScript.sh
+++ .devcontainer/postInstallScript.sh
@@ -1,3 +1,2 @@
-apt-get update && apt-get install make -y
-conda init && conda install python=3.9 -y
-make install-dev
+apt-get update && apt-get install -y make;
+conda init && conda install -y python=3.9;
