1e1fcc2d6db5a4a7dc1350087e49c7c0199f104c
nickolasrm <nickolasrochamachado@gmail.com>
2023-02-08 01:48:07 +0000

    feat: :alembic: added iris bnn experiment
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

---
+++ bnn_analysis/supervised/__init__.py
@@ -0,0 +1,1 @@
+"""Package containing the supervised analysis of the BNNs."""
---
+++ bnn_analysis/supervised/iris_bnn.py
@@ -0,0 +1,33 @@
+"""Train a binarized neural network on the MinMax Scaled Iris dataset."""
+from omegaconf import DictConfig
+from sklearn.preprocessing import MinMaxScaler
+from wandb.keras import WandbMetricsLogger  # pylint: disable=no-name-in-module
+
+from bnn_analysis.base.experiment import experiment
+from bnn_analysis.base.supervised import train
+from bnn_analysis.supervised.iris import IrisDataSet
+
+
+class ScaledIrisDataSet(IrisDataSet):
+    """Get Iris data and splits it into train and test data."""
+
+    def __init__(self):
+        """Scales the original dataset using the train data."""
+        super().__init__()
+        scaler = MinMaxScaler(feature_range=(-1, 1)).fit(self.x_train)
+        self.x_train = scaler.transform(self.x_train)
+        self.x_test = scaler.transform(self.x_test)
+
+
+@experiment
+def iris_bnn(cfg: DictConfig):
+    """Train a BNN model on the MinMax Scaled Iris dataset."""
+    layers = cfg.layers
+    compile = cfg.compile  # pylint: disable=redefined-builtin
+    fit = cfg.fit
+    fit["callbacks"] = [WandbMetricsLogger()]
+    train("iris-bnn", ScaledIrisDataSet(), layers, compile, fit)
+
+
+if __name__ == "__main__":
+    iris_bnn()  # pylint: disable=no-value-for-parameter
