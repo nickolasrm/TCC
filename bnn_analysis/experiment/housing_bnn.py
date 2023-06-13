35ee1d372fa07356edc7c9887b9638379b51ba42
nickolasrm <nickolasrochamachado@gmail.com>
2023-06-13 01:22:59 +0000

    fix: :bug: rebuilt housing bnn exp
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

--- bnn_analysis/experiment/housing_bnn.py
+++ bnn_analysis/experiment/housing_bnn.py
@@ -1,22 +1,12 @@
 """Train a traditional neural network on the Boston Housing dataset."""
 from sklearn.preprocessing import MinMaxScaler
 
-from bnn_analysis.experiment.housing import BostonHousingDataSet
+from bnn_analysis.experiment.housing import StdBostonHousingDataSet
 from bnn_analysis.experiment.supervised import supervised_experiment
 
 
-class ScaledBostonHousingDataSet(BostonHousingDataSet):
-    """Boston Housing data scaled to [-1, 1]."""
-
-    def __init__(self):
-        """Prepare the dataset."""
-        super().__init__()
-        scaler = MinMaxScaler(feature_range=(-1, 1)).fit(self.x_train)
-        self.map_x(scaler.transform)
-
-
 housing_bnn = supervised_experiment(
-    "housing_bnn", ScaledBostonHousingDataSet(), "regression"
+    "housing_bnn", StdBostonHousingDataSet(), "regression"
 )
 
 
